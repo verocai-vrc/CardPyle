1af8421df2645906dcc1cbc102470de428b0f94b
Amelo0 <desousameloarthur@gmail.com>
2025-04-17 10:12:50 -0300

    Card methods and constructor

---
+++ src/card.py
@@ -0,0 +1,46 @@
+class Card:
+    #Construtor da classe carta
+    #name: , strength: força de hit, constitution: vida base, cost: custo de ação, type: carta, feitiço, equiṕamento, terreno
+    
+    def __init__(self, name, strength, constitution, cost, type, tags):
+        self.name = name
+        self.str = strength
+        self.con = constitution
+        self.cost = cost
+        self.type = card_type
+        self.tags = tags if tags else []
+        
+        self.board_position = None
+        self.health = constitution
+        self.owner = None
+    
+    # jogar no tabuleiro
+    def place_on_board(self, board_position, owner):
+        self.board_positiom = position
+        self.owner = owner
+    
+    
+        print(f"{self.name} is placed on {self.board_position}")
+        
+    # atacar
+    def target_and_hit(self, target):
+        print(f"{self.name} attacks {target.name} for {self.str} damage.")
+        target.receive_damage(self.strength)
+        
+    # receber dano
+    def receive_damage(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            self.die()
+        else:
+            print(f"{self.name} has {self.health} health left.")
+        
+    # morrer
+    def die(self):
+        print(f"{self.name} is destroied and off the board.")
+        self.board_position = None
+        move_to_graveyard(self)
+        
+    # mover para o cemiterio
+    def move_to_graveyard(self):
+        print(f"{self.name} is moved to the graveyard.")
