64c0087d2461d738996ef58c4f3e8b22a0cbff89
Arthur Melo <desousameloarthur@gmail.com>
2025-11-19 01:29:51 -0300

    New project structure; innit; yaml; dependencies; card face deck refactors

--- src/CreatureCard.py
+++
@@ -1,6 +0,0 @@
-class CreatureCard(Card):
-    def __init__(self, name, strength, constitution, cost, card_type, tags, board_position=None):
-        super().__init__(name, strength, constitution, cost, card_type, tags, board_position)
-        self.card_type = "Creature"
-        
-        
